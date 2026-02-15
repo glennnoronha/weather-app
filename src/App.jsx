aae4e3ae5a6ff7389a5b6f07e5745d45711549ba
Glenn Noronha <glenn.noronha@yahoo.com>
2026-02-15 15:46:29 -0600

    init react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='bg-gray-950 min-h-screen flex justify-center items-center text-white'>
+      <Weather />
+    </div>
+  )
+}
+
+export default App
